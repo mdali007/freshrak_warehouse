4793ef2465122ccb0f3b343f6da0fc357c00f43e
mdali007 <mdshameerali007@gmail.com>
2024-11-02 16:26:53 +0400

    Add gunicorn to requirements4

--- main/settings.py
+++ main/settings.py
@@ -137,6 +137,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
